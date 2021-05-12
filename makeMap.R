e03b5ba805a769ec54ab0bf1149f46fa68b6a3e3
Ben Teo <bteo@wisc.edu>
2021-05-12 02:26:54 -0500

    Add census blk grp labels to leaflet map

--- makeMap.R
+++ makeMap.R
@@ -1,6 +1,12 @@
 # load CB.lat, CB.lng
 source("loadDataMap.R")
 
+## highlight options (for census block group polygons)
+hl.options <- highlightOptions(color="gold",
+                               fillColor="gold",
+                               weight=2,opacity=0.5,
+                               sendToBack=T)
+
 ## sunprairie map
 m.base <- leaflet() %>% 
   addTiles(options=tileOptions(minZoom=11)) %>%
@@ -33,35 +39,42 @@ m.base <- leaflet() %>%
                    radius=10,fillOpacity=0.5,
                    color="black",fillColor="blue",
                    group="Patrick Marsh") %>%
-  addPolygons(lat=round(CB.lat[1:10],3),lng=round(CB.lng[1:10],3),
-              color="purple",group="Westside",fill=T,
-              weight=3,opacity=0.3,
-              highlightOptions=highlightOptions(color="gold",
-                                                fillColor="gold",
-                                                weight=2,opacity=0.5,
-                                                sendToBack=T)) %>%
-  addPolygons(lat=round(CB.lat[11:25],3),lng=round(CB.lng[11:25],3),
-              color="green",group="Northside",fill=T,
-              weight=3,opacity=0.3,
-              highlightOptions=highlightOptions(color="gold",
-                                                fillColor="gold",
-                                                weight=2,opacity=0.5,
-                                                sendToBack=T)) %>%
-  addPolygons(lat=round(CB.lat[26:30],3),lng=round(CB.lng[26:30],3),
-              color="red",group="CH Bird",fill=T,
-              weight=3,opacity=0.3,
-              highlightOptions=highlightOptions(color="gold",
-                                                fillColor="gold",
-                                                weight=2,opacity=0.5,
-                                                sendToBack=T)) %>% 
-  addPolygons(lat=round(CB.lat[11:45],3),lng=round(CB.lng[11:45],3),
-              color="blue",group="Patrick Marsh",fill=T,
-              weight=3,opacity=0.3,
-              labelOptions=labelOptions(permanent=T),
-              highlightOptions=highlightOptions(color="gold",
-                                                fillColor="gold",
-                                                weight=2,opacity=0.5,
-                                                sendToBack=T)) %>% 
+  addPolygons(lat=round(CB.lat[1:4],3),lng=round(CB.lng[1:4],3),
+              color="purple",group="Westside",
+              opacity=0.3,label="011506-2",
+              highlightOptions=hl.options) %>%
+  addPolygons(lat=round(CB.lat[6:9],3),lng=round(CB.lng[6:9],3),
+              color="purple",group="Westside",
+              opacity=0.3,label="011505-1",
+              highlightOptions=hl.options) %>%
+  addPolygons(lat=round(CB.lat[11:14],3),lng=round(CB.lng[11:14],3),
+              color="green",group="Northside",
+              opacity=0.3,label="011505-1",
+              highlightOptions=hl.options) %>%
+  addPolygons(lat=round(CB.lat[16:19],3),lng=round(CB.lng[16:19],3),
+              color="green",group="Northside",
+              opacity=0.3,label="011505-3",
+              highlightOptions=hl.options) %>%
+  addPolygons(lat=round(CB.lat[21:24],3),lng=round(CB.lng[21:24],3),
+              color="green",group="Northside",
+              opacity=0.3,label="011506-3",
+              highlightOptions=hl.options) %>%
+  addPolygons(lat=round(CB.lat[26:29],3),lng=round(CB.lng[26:29],3),
+              color="red",group="CH Bird",
+              opacity=0.3,label="011504-1",
+              highlightOptions=hl.options) %>% 
+  addPolygons(lat=round(CB.lat[31:34],3),lng=round(CB.lng[31:34],3),
+              color="blue",group="Patrick Marsh",
+              opacity=0.3,label="011600-2",
+              highlightOptions=hl.options) %>% 
+  addPolygons(lat=round(CB.lat[36:39],3),lng=round(CB.lng[36:39],3),
+              color="blue",group="Patrick Marsh",
+              opacity=0.3,label="011700-2",
+              highlightOptions=hl.options) %>%
+  addPolygons(lat=round(CB.lat[41:44],3),lng=round(CB.lng[41:44],3),
+              color="blue",group="Patrick Marsh",
+              opacity=0.3,label="011506-1",
+              highlightOptions=hl.options) %>%
   addLayersControl(
     overlayGroups=c("Landmarks","Westside","Northside",
                     "CH Bird","Patrick Marsh"),
